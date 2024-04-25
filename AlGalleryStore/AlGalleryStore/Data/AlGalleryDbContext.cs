cabfcd50bc552182f72422ffaffe5ef35f47d7bb
Eng. Hadeer Badr <Hadeerb47@gmail.com>
2024-04-25 21:15:12 +0200

    Created The Main Structure

---
+++ AlGalleryStore/AlGalleryStore/Data/AlGalleryDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace AlGalleryStore.Data
+{
+    public class AlGalleryDbContext : DbContext
+    {
+        public AlGalleryDbContext(DbContextOptions<AlGalleryDbContext>options):base(options) 
+        {
+                
+        }
+    }
+}
---
+++ AlGalleryStore/AlGalleryStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlGalleryStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
